f3542be8c86e01b1cf8fb22d8186f6e24614ccaa
aizablanco <aizablnco@gmail.com>
2025-09-15 15:04:37 +0800

    updated

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'https://dilay-aiza-1.onrender.com';
+$config['base_url'] 				= 'https://dilay-aiza-1.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
