29bc3bfe9c999b7f384d860e264d1b8839c5f44d
caegstephanie <stephaniecaeg@gmail.com>
2025-09-19 09:27:39 +0800

    hghg

--- app/config/routes.php
+++ app/config/routes.php
@@ -43,10 +43,10 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |
 */
 
-$router->get('/', 'Welcome::index');
-$router->get('/about', 'Welcome::about');
+//$router->get('/', 'Welcome::index');
+//$router->get('/about', 'Welcome::about');
 $router->get('/user/profile/{username}/{name}', 'UserController::profile');
-$router->get('/user/show', 'UserController::show');
+$router->get('/', 'UserController::show');
 $router->match('/user/create', 'UserController::create', ['GET', 'POST']);
 $router->match('/user/update/{id}', 'UserController::update', ['GET', 'POST']);
 $router->get('/user/delete/{id}', 'UserController::delete');
